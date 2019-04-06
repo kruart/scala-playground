14d140ce07fce102ca2c85ea9b66b0c8e1e251af
arthur.krukowski <weoz@ukr.net>
2019-04-06 22:46:36 +0300

    rock-the-jvm-scala-beginners: 3.4 method notations(infix, prefix, postfix) and apply method (exercises)

--- rock-the-jvm-scala-beginners/src/lectures/part2oop/MethodNotations.scala
+++ rock-the-jvm-scala-beginners/src/lectures/part2oop/MethodNotations.scala
@@ -1,12 +1,18 @@
 package lectures.part2oop
 
 object MethodNotations extends App {
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(nickname: String): Person = new Person(s"$name $nickname", favoriteMovie)
     def unary_! : String = s"$name, what the heck?!"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+    def apply(n: Int): String = s"$name watched $favoriteMovie $n times"
+
+    def learns(thing: String): String = s"$name learns $thing"
+    def learnsScala: String = this.learns("Scala")
   }
 
   val mary = new Person("Mary", "Inception")
@@ -40,4 +46,36 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary()) //equivalent
+
+  /*
+    1. Overload the + operator
+       mary + "the rockstar" => new person "Mary (the rockstar)"
+
+    2. Add an age to the person class
+       Add a unary + operator => new person with the age + 1
+       +mary => mary with the age incrementer
+
+    3. Add a "learns" method in the Person class => "Mary learns Scala"
+       Add a learnsScala method, calls learns method with "Scala".
+       Use it in postfix notation.
+
+    4. Overload the apply method
+       mary.apply(2) => "Mary watched Inception 2 times"
+   */
+
+  // 1
+  println((mary + "the rockstar")())
+  println((mary + "the rockstar").apply())  // equivalent
+
+  // 2
+  println((+mary).age)  // 1
+  println((+(+mary)).age)  // 2
+
+  // 3
+  println(mary learns "Java")
+  println(mary learnsScala)
+
+  // 4
+  println(mary(5))
+  println(mary.apply(5))  // equivalent
 }
