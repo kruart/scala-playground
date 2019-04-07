14094153777b34e49f1a8258cb1c70161a454fc7
arthur.krukowski <weoz@ukr.net>
2019-04-07 15:28:07 +0300

    akka-http-quickstart: creating custom directives

--- akka-http-quickstart/src/main/scala/Router.scala
+++ akka-http-quickstart/src/main/scala/Router.scala
@@ -1,32 +1,31 @@
 import akka.http.scaladsl.server.{Directives, Route}
 
-import scala.util.{Failure, Success}
-
 trait Router {
   def route: Route
 }
 
-class TodoRouter(todoRepository: TodoRepository) extends Router with Directives {
+class TodoRouter(todoRepository: TodoRepository) extends Router with Directives with TodoDirectives {
   import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
   import io.circe.generic.auto._
 
   override def route: Route = pathPrefix("todos") {
     pathEndOrSingleSlash {
       get {
-        onComplete(todoRepository.all()) {
-          case Success(todos) => complete(todos)
-          case Failure(exception) =>
-            println(exception.getMessage)
-            complete(ApiError.generic.statusCode, ApiError.generic.message)
+        handleWithGeneric(todoRepository.all()) { todos =>
+          complete(todos)
         }
       }
     } ~ path("done") {
       get {
-        complete(todoRepository.done())
+        handleWithGeneric(todoRepository.done()) { todos =>
+          complete(todos)
+        }
       }
     } ~ path("pending") {
       get {
-        complete(todoRepository.pending())
+        handleWithGeneric(todoRepository.pending()) { todos =>
+          complete(todos)
+        }
       }
     }
   }
