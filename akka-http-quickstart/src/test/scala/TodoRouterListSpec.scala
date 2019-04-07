1ab9e5c783ce3a25096e6afc772fb7bc7a59bff7
arthur.krukowski <weoz@ukr.net>
2019-04-07 15:25:44 +0300

    akka-http-quickstart: failing repository and tests

--- akka-http-quickstart/src/test/scala/TodoRouterListSpec.scala
+++ akka-http-quickstart/src/test/scala/TodoRouterListSpec.scala
@@ -2,7 +2,7 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.{Matchers, WordSpec}
 
-class TodoRouterListSpec extends WordSpec with Matchers with ScalatestRouteTest {
+class TodoRouterListSpec extends WordSpec with Matchers with ScalatestRouteTest with TodoMocks {
   import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
   import io.circe.generic.auto._
 
@@ -42,6 +42,33 @@ class TodoRouterListSpec extends WordSpec with Matchers with ScalatestRouteTest 
         status shouldBe StatusCodes.OK
         val response = responseAs[Seq[Todo]]
         response shouldBe Seq(pendingTodo)
+      }
+    }
+
+    "handle repository failure in the todos" in {
+      val repository = new FailingRepository
+      val router = new TodoRouter(repository)
+
+      Get("/todos") ~> router.route ~> check {
+        status shouldBe StatusCodes.InternalServerError
+      }
+    }
+
+    "handle repository failure in the done todos route" in {
+      val repository = new FailingRepository
+      val router = new TodoRouter(repository)
+
+      Get("/todos/done") ~> router.route ~> check {
+        status shouldBe StatusCodes.InternalServerError
+      }
+    }
+
+    "handle repository failure in the pending todos route" in {
+      val repository = new FailingRepository
+      val router = new TodoRouter(repository)
+
+      Get("/todos/pending") ~> router.route ~> check {
+        status shouldBe StatusCodes.InternalServerError
       }
     }
   }
